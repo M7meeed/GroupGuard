eeaf1a91e8dd51cfdbc398fc0a67fa6693c5f2bb
M7med <mohd.qh11@gmail.com>
2026-03-14 02:50:54 +0300

    add chromium support

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ]
     }
 });
 
